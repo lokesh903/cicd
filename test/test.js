d89d986ae7cb51aad0a8f34d82185733bdf0f7d1
lokesh903 <lavkeshchhatani@gmail.com>
2024-12-12 17:31:15 +0530

    test2

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ const request = require("supertest");
 const app = require("../index");
 describe("GET /", () => {
   it("respond with Server is running", (done) => {
-    request(app).get("/").expect("Server is running test1", done);
+    request(app).get("/cicd-test/").expect("Server is running test1", done);
   })
 });
