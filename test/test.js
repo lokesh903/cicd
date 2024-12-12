787a9faedff41bc27a0539fa6c469af31bd21d11
lokesh903 <lavkeshchhatani@gmail.com>
2024-12-12 17:27:37 +0530

    test1

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ const request = require("supertest");
 const app = require("../index");
 describe("GET /", () => {
   it("respond with Server is running", (done) => {
-    request(app).get("/").expect("HServer is running", done);
+    request(app).get("/").expect("Server is running test1", done);
   })
 });
